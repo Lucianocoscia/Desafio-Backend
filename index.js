f44123d5c7701da8de41dfdb569377188b413d13
Luciano Coscia <lucianocoscia5@gmail.com>
2022-10-27 23:07:11 -0300

    Cambiando de nombre parametros

--- index.js
+++ index.js
@@ -20,18 +20,18 @@ class Usuario {
         //retorna la cantidad de mascotas q tiene el usuario
         return this.mascotas.length
     }
-    addBook(titulo, autor){
+    addBook(nombre, autor){
         //recibe un string nombre y un string autor y debe agregar un objeto {nombre: string, autor: string} ala rray de libros 
-        this.libros.push({titulo, autor});
+        this.libros.push({nombre, autor});
     }
     getBookNames(){
         //retorna un array con solo los nombres del array de librops del usuario.
-        return this.libros.map(e=> e.titulo)
+        return this.libros.map(e=> e.nombre)
     }
 
 }
 // Punto 4: Crear un objeto llanmado usuario con valores arbitrarios e invocar todos sus metodos.
-let nuevoUsuario = new Usuario('Luciano', 'Coscia',[{titulo: 'Harry Potter', autor: 'Juan Perez'}], ['Perro'] );
+let nuevoUsuario = new Usuario('Luciano', 'Coscia',[{nombre: 'Harry Potter', autor: 'Juan Perez'}], ['Perro'] );
 console.log(nuevoUsuario.getfullName());
 
 nuevoUsuario.addMascota('gato');
